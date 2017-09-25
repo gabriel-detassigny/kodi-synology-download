b832cb9be424bec0e92ba7e410ff4b1b729b8cb5
Gabriel de Tassigny <gabriel@multivac.cc>
2017-09-25 23:17:14 +0100

    Small tweaks in UI and adding some constants

--- resources/lib/kodigui.py
+++ resources/lib/kodigui.py
@@ -1,19 +1,29 @@
 import xbmcgui
 
 class TaskListWindow(xbmcgui.Window):
+    X_TITLE = 190
+    X_STATUS = 800
+    X_PERCENT = 980
+
     def add_headers(self):
-        self.addControl(xbmcgui.ControlLabel(x=190, y=25, width=600, height=50, label='Title'))
-        self.addControl(xbmcgui.ControlLabel(x=800, y=25, width=100, height=50, label='Status'))
-        self.addControl(xbmcgui.ControlLabel(x=920, y=25, width=150, height=50, label='Percentage'))
+        self.addControl(xbmcgui.ControlLabel(
+            x=TaskListWindow.X_TITLE, y=25, width=600, height=50, label='Title'))
+        self.addControl(xbmcgui.ControlLabel(
+            x=TaskListWindow.X_STATUS, y=25, width=150, height=50, label='Status'))
+        self.addControl(xbmcgui.ControlLabel(
+            x=TaskListWindow.X_PERCENT, y=25, width=150, height=50, label='Percentage'))
 
     def add_tasks(self, taskList):
         y = 75
         for task in taskList:
             y += 30
-            self.addControl(xbmcgui.ControlLabel(x=190, y=y, width=600, height=25, label=task['title']))
-            self.addControl(xbmcgui.ControlLabel(x=800, y=y, width=100, height=25, label=task['status']))
             percent = self.__get_download_percent_label(task)
-            self.addControl(xbmcgui.ControlLabel(x=920, y=y, width=150, height=25, label=percent))
+            self.addControl(xbmcgui.ControlLabel(
+                x=TaskListWindow.X_TITLE, y=y, width=600, height=25, label=task['title']))
+            self.addControl(xbmcgui.ControlLabel(
+                x=TaskListWindow.X_STATUS, y=y, width=150, height=25, label=task['status']))
+            self.addControl(xbmcgui.ControlLabel(
+                x=TaskListWindow.X_PERCENT, y=y, width=150, height=25, label=percent))
 
     def __get_download_percent_label(self, task):
         if task['size'] == 0:
